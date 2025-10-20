e97b602c3d3af6893b4e7e6290cd4973a0763ce0
ilviainanza <ilviainanza3@gmail.com>
2025-10-20 10:47:39 +0800

    latihan

---
+++ latihan3_polymorphism.js
@@ -0,0 +1,32 @@
+class Payment {
+  process(amount) {
+    console.log(`Memproses pembayaran sebesar Rp${amount.toLocaleString}`);
+  }
+}
+
+class CreditCardPayment extends Payment {
+  process(amount) {
+    console.log(`Membayar Rp${amount} dengan kartu kredit.`);
+  }
+}
+
+class EWalletPayment extends Payment {
+  process(amount) {
+    console.log(`Membayar Rp${amount} dengan e-wallet.`);
+  }
+}
+
+class BankTransferPayment extends Payment {
+  process(amount) {
+    console.log(`Membayar Rp${amount} lewat transfer bank.`);
+  }
+}
+
+const creditCard = new CreditCardPayment();
+creditCard.process(250000); 
+
+const ewallet = new EWalletPayment();
+ewallet.process(100000); 
+
+const bankTransfer = new BankTransferPayment();
+bankTransfer.process(750000); 
